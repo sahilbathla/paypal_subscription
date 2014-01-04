46b3eb50b49ba6f6d16bcbb5eadb3db9cb61d300
Sahil <sahil@vinsol.com>
2014-01-04 10:59:20 +0530

    Only sandbox paypal in all env

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,3 +1,4 @@
 $(document).ready(function(){
   $('#return').val($('#return').val() + '/' + $('#auth-token').attr('data-token'));
+  $('#return').parent('form').attr('action','https://www.sandbox.paypal.com/cgi-bin/webscr');
 })
